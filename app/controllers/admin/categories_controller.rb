54335c35e1c3c6d8d2999c1ddb908836284d39e5
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-11-17 14:09:28 +0000

    added form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,9 +13,18 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :categories], notice: 'Category created!'
+      redirect_to [:admin, :products], notice: 'Category created!'
     else
       render :new_category
     end
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
 end
