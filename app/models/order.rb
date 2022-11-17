c228f9f3a03a30c74ff72a509686b498b0ee641e
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-11-17 00:09:02 +0000

    added feature

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   
+  # this makes it possible to do order,line_items in controller which gets all the line items for a specific order number
   has_many :line_items
 
   monetize :total_cents, numericality: true
