53aff7abee113afbd15aa4ba587812618950ef13
Anouk Van Bogaert <vb.anouk@gmail.com>
2022-11-23 08:52:16 +0000

    added product details spec

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -7,11 +7,16 @@ describe('home app', () => {
     cy.visit('http://0.0.0.0:3000/');
   });
 
-  it("visits the home page", () => {
-    cy.visit("http://0.0.0.0:3000/");
-  });
-
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+
+  it("shows the product details when clicked", () => {
+    cy.get(".products article")
+      .first()
+      .click();
+    cy.get('article')
+      .should('have.class', 'product-detail');
+  });
+
 });
